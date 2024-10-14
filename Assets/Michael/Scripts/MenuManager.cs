e22d68039b94e150cc854ed82dbe725ba00ec420
Michael-elie <michael-elie@hotmail.com>
2024-10-14 17:08:56 +0200

    Update

--- Assets/Michael/Scripts/MenuManager.cs
+++ Assets/Michael/Scripts/MenuManager.cs
@@ -1,11 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuManager : MonoBehaviour
 {
+    public static float MusicVolume;
+    public static float SfxVolume;
+    [SerializeField] private AudioMixer _mixer;
+    [SerializeField] private Slider _sfxSlider;
+    [SerializeField] private Slider _musicSlider;
     [SerializeField] private List<GameObject> _menuPanels;
+    
     void Start()
     {
         
@@ -21,6 +29,18 @@ public class MenuManager : MonoBehaviour
             menuPanel.SetActive(panel == menuPanel);
         }
     }
+    public void SetMusicVolume()
+    {
+        MusicVolume = _musicSlider.value;
+        _mixer.SetFloat("Music", Mathf.Log10(MusicVolume) * 20);
+    }
 
+    public void SetSfxVolume()
+    {
+        SfxVolume = _sfxSlider.value;
+        _mixer.SetFloat("Sfx", Mathf.Log10(SfxVolume) * 20);
+    }
+    
+    
     
 }
